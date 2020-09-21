828e1584bcc7d6b6142d570f0ca278bc886f4fd5
Juan Magnarelli <juanmagnarelli@gmail.com>
2020-09-21 03:24:59 -0300

    Path raiz

--- web/api/urls.py
+++ web/api/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from .views import solicitud, pedidos, login, register
 
 urlpatterns = [
+    url('', solicitud, name='solicitudHome'),
     url('solicitud/', solicitud, name='solicitud'),
     url('pedidos/', pedidos, name='pedidos'),
     url('login/', login, name='login'),
